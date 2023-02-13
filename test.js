bab75c05ae1b7391e54798c66c6e1f850730c5a0
andronedev <andronedev@gmail.com>
2023-02-13 16:29:22 +0100

    fix 3 test 2

--- test.js
+++ test.js
@@ -1,5 +1,3 @@
-document.body.addEventListener('onload', function() {
-    document.querySelector("#app > div > ion-nav > div > ion-app > ion-split-pane > ion-menu > ion-header > ion-toolbar > div > div").innerHTML+=`
+document.querySelector("#app > div > ion-nav > div > ion-app > ion-split-pane > ion-menu > ion-header > ion-toolbar > div > div").innerHTML+=`
 <span>Extension</span>
 `
-})
